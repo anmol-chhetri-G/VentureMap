59c6dd8196ae92f79a2fb5152a0a8115bf303775
anmol <anmol@debian-btw.ip6.wlink.com.np>
2026-02-27 00:23:47 +0545

    gitignore updated for pycache

--- backend/models.py
+++ backend/models.py
@@ -4,7 +4,7 @@ from sqlalchemy.sql import func
 from database import Base
 import enum
 
-# Define the status options for Draft Persistence [cite: 56]
+
 class IncorporationStatus(enum.Enum):
     draft = "draft"
     complete = "complete"
@@ -13,24 +13,24 @@ class Company(Base):
     __tablename__ = "companies"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False) # Requirement: Company Name [cite: 21]
-    shareholder_count = Column(Integer, default=0) # Requirement: Number of Shareholders [cite: 22]
-    total_capital = Column(Numeric(10, 2), default=0.00) # Requirement: Total Capital [cite: 23]
-    status = Column(Enum(IncorporationStatus), default=IncorporationStatus.draft) # Requirement: Draft Saved 
+    name = Column(String, nullable=False) 
+    shareholder_count = Column(Integer, default=0) 
+    total_capital = Column(Numeric(10, 2), default=0.00) 
+    status = Column(Enum(IncorporationStatus), default=IncorporationStatus.draft) 
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
-    # One-to-Many Relationship: One company can have multiple shareholders [cite: 52]
+    # One-to-Many Relationship: One company can have multiple shareholders
     shareholders = relationship("Shareholder", back_populates="company", cascade="all, delete-orphan")
 
 class Shareholder(Base):
     __tablename__ = "shareholders"
 
     id = Column(Integer, primary_key=True, index=True)
-    first_name = Column(String, nullable=False) # Requirement: First Name [cite: 28]
-    last_name = Column(String, nullable=False) # Requirement: Last Name [cite: 29]
-    nationality = Column(String, nullable=False) # Requirement: Nationality [cite: 30]
-    
+    first_name = Column(String, nullable=False) 
+    last_name = Column(String, nullable=False) 
+    nationality = Column(String, nullable=False) 
+
     # Foreign Key linking to the Company 
     company_id = Column(Integer, ForeignKey("companies.id", ondelete="CASCADE"))
     
